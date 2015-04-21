74b204a980edb1813fba4d659e9ab26601c8794f
rkalnik <rkalnik@euromoneyplc.com>
2015-04-21 16:01:38 +0200

    ConsoleApplication_test1 & WindowsFormsApplicationTest1

--- WindowsFormsApplicationTest1/WindowsFormsApplicationTest1/Program.cs
+++ WindowsFormsApplicationTest1/WindowsFormsApplicationTest1/Program.cs
@@ -6,6 +6,11 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplicationTest1
 {
+	class Foo
+	{
+		public string About = "Klasa v.1.0";
+	}
+	
 	static class Program
 	{
 		/// <summary>
@@ -17,6 +22,10 @@ namespace WindowsFormsApplicationTest1
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new Form1());
+			
+			Foo MyFoo = new Foo();
+			Console.Write(MyFoo.About);
+			Console.Read();
 		}
 	}
 }
