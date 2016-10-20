2a688fa9e1fd830fd57a68ebf552ed7e294537a6
rkalnik <rkalnik@euromoneyplc.com>
2016-10-20 14:34:30 +0200

    Reading HTML tables

---
+++ Training/Selenium/ReadingHtmlTables/ReadingHtmlTables/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using System.Threading;
+
+namespace ReadingHtmlTables
+{
+	class Program : Base
+	{
+
+		static void Main(string[] args)
+		{
+			Initilize();
+			TablePage page = new TablePage();
+
+			//Read table
+			Utilities.ReadTable(page.table);
+			Console.WriteLine("********************");
+			//Get the cell value from the table
+			Console.WriteLine(Utilities.ReadCell("First name", 2));
+			Console.WriteLine("********************");
+			Console.ReadKey();
+		}
+
+		[SetUp]
+		public static void Initilize()
+		{
+			driver = new ChromeDriver();
+			driver.Manage().Window.Maximize();
+			driver.Navigate().GoToUrl("http://codepen.io/jordyvanraaij/full/jlAqp/");
+		}
+
+		[Test]
+		public void GetHtmlTable()
+		{
+			//TablePage page = new TablePage();
+
+			////Read table
+			//Utilities.ReadTable(page.table);
+
+			////Get the cell value from the table
+			//Utilities.ReadCell("First name", 2);
+
+
+
+		}
+
+		[TearDown]
+
+		public void Close()
+		{
+			driver.Quit();
+		}
+	}
+}
---
+++ Training/Selenium/ReadingHtmlTables/ReadingHtmlTables/Utilities.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace ReadingHtmlTables
+{
+	public class Utilities
+	{
+
+		static List<TableDatacollection> _tableDatacollection = new List<TableDatacollection>();
+
+		public static void ReadTable(IWebElement table)
+		{
+			//Get all the rows
+			var rows = table.FindElements(By.TagName("tr"));
+
+			//Get all columns from the table
+			var columns = table.FindElements(By.TagName("th"));
+					
+
+			//Create row index
+			int rowIndex = 0;
+
+			foreach (var row in rows)
+			{
+				int colIndex = 0;
+
+				var colData = row.FindElements(By.TagName("td"));
+
+				foreach(var colValue in colData)
+				{
+					_tableDatacollection.Add(new TableDatacollection()
+					{
+						RowNumber = rowIndex,
+						ColumnName = columns[colIndex].Text,
+						ColumnValue = colValue.Text
+					});
+
+					//Move to next column
+					colIndex++;
+				}
+				rowIndex++;
+			}
+
+		}
+
+		public static string ReadCell(string columnName, int rowNumber)
+		{
+			var data = (from e in _tableDatacollection
+						where e.ColumnName == columnName && e.RowNumber == rowNumber
+						select e.ColumnValue).SingleOrDefault();
+
+			return data;
+		}
+
+	}
+
+	public class TableDatacollection
+	{
+		public int RowNumber { get; set; }
+		public string ColumnName { get; set; }
+		public string ColumnValue { get; set; }
+	}
+
+}
