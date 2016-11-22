1ae990b57ba292a1eed910df0386407011ec09dc
rkalnik <rkalnik@euromoneyplc.com>
2016-11-22 11:41:28 +0100

    Roz 7 Interfejcy tbc

---
+++ Training/C#_HeadFirst/Rozdzial_7/System zarzadzania ulem/SystemZarzadzaniaUlemCz3Interfejsy/SystemZarzadzaniaUlem/NectarCollector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemZarzadzaniaUlem
+{
+	class NectarCollector : Worker
+	{
+		public NectarCollector(string[] jobsICanDo, double weightMg) : base(jobsICanDo, weightMg)
+		{
+		}
+
+		public int Nectar { get; set; }
+		public void FindFlowers()
+		{
+
+		}
+
+		public void GatherNectar()
+		{
+
+		}
+
+		public void ReturnToHive()
+		{
+
+		}
+	}
+}
