9d168b8777c93eb129c710fdfa6afa124f0950fc
rkalnik <rkalnik@euromoneyplc.com>
2018-08-03 13:55:48 +0200

    Added CarbonFootprint to 10-Introduction OPP

---
+++ Training/Selenium/UdemySeleniumC/10-Introduction OOP/CarbonFootprint/CarbonFootprint/Bicycle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarbonFootprint
+{
+	class Bicycle : ICarbonFootprint
+	{
+		public void CalcCarbonFootprint()
+		{
+			Console.WriteLine("A bicycle has zero carbon footprint");
+		}
+	}
+}
